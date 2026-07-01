7bcdcfb8f7941f66f0edc6eac151af624fccdac2
JuanMazza91 <juance_t@hotmail.com>
2026-07-01 05:40:47 -0300

    Arreglando problemas de conexion

--- schemas/usuario.schema.js
+++ schemas/usuario.schema.js
@@ -4,7 +4,7 @@ const usuarioSchema = Joi.object({
   nickname: Joi.string().min(3).max(50).required().messages({
     "string.min": "El nickname debe tener al menos 3 caracteres",
     "string.max": "El nickname no puede superar los 50 caracteres",
-    "any.required": "El nickname  es obligatorio",
+    "any.required": "El nickname es obligatorio",
   }),
   email: Joi.string().email().required().messages({
     "string.email": "El email no tiene un formato válido",
@@ -14,9 +14,12 @@ const usuarioSchema = Joi.object({
     "string.min": "La contraseña debe tener al menos 6 caracteres",
     "any.required": "La contraseña es obligatoria",
   }),
+  avatar: Joi.string().optional().messages({
+    "string.base": "El avatar debe ser un texto válido con el nombre del archivo",
+  })
 });
 
-//Para que se pueda actualizar los campos por separados del usuario sin necesidad de tener que actualizar todos cada vez que se quiere actualizar algo
+
 const actualizarUsuarioSchema = Joi.object({
   nickname: Joi.string().min(3).max(50).messages({
     "string.min": "El nickname debe tener al menos 3 caracteres",
@@ -28,6 +31,9 @@ const actualizarUsuarioSchema = Joi.object({
   password: Joi.string().min(6).messages({
     "string.min": "La contraseña debe tener al menos 6 caracteres",
   }),
+  avatar: Joi.string().optional().messages({
+    "string.base": "El avatar debe ser un texto válido",
+  })
 })
   .min(1)
   .messages({
